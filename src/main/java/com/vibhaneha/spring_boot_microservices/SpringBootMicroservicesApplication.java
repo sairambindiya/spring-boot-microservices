40978d12aced1ae309df00e36ba64642426a0453
Bindiya <bindiyam87@gmail.com>
2024-06-24 21:31:36 +0530

    Adding catalog-service module setup

---
+++ src/main/java/com/vibhaneha/spring_boot_microservices/SpringBootMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.vibhaneha.spring_boot_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservicesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vibhaneha/spring_boot_microservices/SpringBootMicroservicesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vibhaneha.spring_boot_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
